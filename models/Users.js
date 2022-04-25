d7a8e9f06e3d1823ae6a1140eda7cb2dd70bf9b4
icesplendent <icesplendent@gmail.com>
2022-04-25 22:09:57 +0800

    feat: add userSchema

---
+++ models/Users.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  passward: {
+    type: String,
+    required: true
+  },
+  goodFile: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'File' // fileSchema
+  },
+  questions: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'FAQ'
+  }
+});
+
+UserSchema.index({ username: 1 });
+
+export default mongoose.model('User', UserSchema);
